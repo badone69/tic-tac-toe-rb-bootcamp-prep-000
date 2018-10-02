2f83154552810ed5ed8ef4ef1414a59b22c2373c
student online <eliel.badon@gmail.com>
2018-10-02 18:42:32 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,6 +29,21 @@ def position_taken?(board, location)
   board[location] != " " && board[location] != ""
 end
 
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
   
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
